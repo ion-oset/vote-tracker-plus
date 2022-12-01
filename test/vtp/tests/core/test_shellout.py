5b92b4062ad4dbc109e148df962a9bbb43d9a2fa
Ion Y <ion@osetinstitute.org>
2022-12-01 11:17:01 -0800

    Make tests of 'Shellout.run' more general.

--- test/vtp/tests/core/test_shellout.py
+++ test/vtp/tests/core/test_shellout.py
@@ -1,5 +1,7 @@
 import pytest
 
+import sys
+
 from vtp.core.common import Shellout
 
 from tests.utils import raises, raises_none
@@ -7,11 +9,66 @@ from tests.utils import raises, raises_none
 
 # --- Test data
 
+# Notes
+# - All these tests use 'echo' because it will exist where Bash does, and
+#   can't fail (the way 'ls' could if it was run with a non-existent path),
+# - Doing this right needs mocks of file paths.
 
-# args, stdout, stderr
+# TODO:
+# - Actual stderr tests
+# - Test 'Shellout.changed_cwd'
+# - Test 'Shellout.changed_branch'
+# - Test 'Shellout.cvr_parse_git_log_output'
+
+# args, kwargs, stdout, stderr
 SHELLOUT_RUN_TESTS = [
-    ( ["echo", 101], "101", "" ),
-    ( ["echo", "1..2..3"], "1..2..3", "" )
+    (
+        [ "echo", "1..2..3" ],
+        {},
+        "1..2..3",
+        "",
+    ),
+    (
+        [ "echo", 101 ] ,
+        {},
+        "101",
+        ""
+    ),
+    (
+        [ "echo", "1..2..3" ],
+        { "no_touch_stds": False },
+        "1..2..3",
+        ""
+    ),
+    (
+        [ "echo", "1..2..3" ],
+        { "no_touch_stds": True },
+        "1..2..3",
+        ""
+    ),
+    (
+        [ "echo", "1..2..3" ],
+        { "verbosity": 2 },
+        "",
+        ""
+    ),
+    # 'stdout' == None is the default, so sys.stdout is used
+    (
+        [ "echo", "1..2..3" ],
+        { "stdout": None, "verbosity": 2 },
+        "1..2..3",
+        ""
+    ),
+    # This fails because sys.stdout is not a file descriptor, and not because
+    # the expected output is wrong. This is an artifact of using 'capfd' instead
+    # of 'capsys' in the test.
+    pytest.param(
+        [ "echo", "1..2..3" ],
+        { "stdout": sys.stdout, "verbosity": 2 },
+        "1..2..3",
+        "",
+        marks = pytest.mark.xfail
+    ),
 ]
 
 
@@ -22,39 +79,23 @@ SHELLOUT_RUN_TESTS = [
 # - PyTest captures all output. To test for 'Shellout' output use the 'capfd'
 #   fixture's output and error, not the results of 'Shellout.run' (which will
 #   always be None).
+# - 'capfd' doesn't allow passing streams to output and errors (need 'capsys'
+#   for that), which makes it hard to write to stderr instead of stdout.
 # - Parameters starting with '_' in test data but ignored by the test case.
 
-@pytest.mark.parametrize("args,_output,_errors", SHELLOUT_RUN_TESTS)
-def test_shellout_run_output(args, _output, _errors, capfd):
-    # 'Shellout.run' discards output & errors by default
-    assert Shellout.run(args).stdout == None
-    actual = capfd.readouterr().out.rstrip()
-    expected = _output
-    assert(actual == expected)
-
-
-@pytest.mark.parametrize("args,_output,_errors", SHELLOUT_RUN_TESTS)
-def test_shellout_run_errors(args, _output, _errors, capfd):
-    # 'Shellout.run' discards output & errors by default
-    assert Shellout.run(args).stderr == None
-    actual = capfd.readouterr().err.rstrip()
-    expected = _errors
-    assert(actual == expected)
-
-
-@pytest.mark.parametrize("args,output,_errors", SHELLOUT_RUN_TESTS)
-def test_shellout_run_with_io_output(args, output, _errors, capfd):
-    Shellout.run(args, no_touch_stds = True)
-    # Drop the trailing newline
+@pytest.mark.parametrize("args,kwargs,output,_errors", SHELLOUT_RUN_TESTS)
+def test_shellout_run_output(args, kwargs, output, _errors, capfd):
+    Shellout.run(args, **kwargs).stdout == None
+    # Drop any trailing newline
     actual = capfd.readouterr().out.rstrip()
     expected = output
     assert(actual == expected)
 
 
-@pytest.mark.parametrize("args,_output,errors", SHELLOUT_RUN_TESTS)
-def test_shellout_run_with_io_errors(args, _output, errors, capfd):
-    Shellout.run(args, no_touch_stds = True)
-    # Drop the trailing newline
+@pytest.mark.parametrize("args,kwargs,_output,errors", SHELLOUT_RUN_TESTS)
+def test_shellout_run_errors(args, kwargs, _output, errors, capfd):
+    assert Shellout.run(args, **kwargs).stderr == None
+    # Drop any trailing newline
     actual = capfd.readouterr().err.rstrip()
     expected = errors
     assert(actual == expected)
